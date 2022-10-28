1fb54d22612654f5660b0fc1935dba64dbdd2e78
marcelmacedo <marcel.handock@gmail.com>
2022-10-28 10:46:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-1yr5ux
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-1yr5ux)
---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-component',
+  template: '<h3>Marcel</h3>',
+  styles: ['h3{color: blue}'],
+})
+export class MenuComponent {}
